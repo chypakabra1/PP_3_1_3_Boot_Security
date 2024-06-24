cf0bef8f09a7985ee208ffe8526d027be0529ad8
chypakabra1 <157878299+chypakabra1@users.noreply.github.com>
2024-06-24 12:37:38 +0300

    Add files via upload

---
+++ PP_3_1_3_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SuccessUserHandler successUserHandler;
+
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserDetailsService userDetailsService) {
+        this.successUserHandler = successUserHandler;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/authenticated/**").authenticated()
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/only_for_admins/**").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/read_profile/**").hasAuthority("READ_PROFILE")
+                .and()
+                .formLogin().successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoauthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
+}
---
+++ PP_3_1_3_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserServiceImp userServiceImp;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserServiceImp userServiceImp, RoleService roleService) {
+        this.userServiceImp = userServiceImp;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("users", userServiceImp.index());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("user", userServiceImp.findById(id));
+        return "show";
+    }
+
+    @GetMapping("/create")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleService.findAll());
+        return "new";
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @RequestParam("selectedRoles") List<Long> selectedRoles) {
+        if (bindingResult.hasErrors()) {
+            return "new";
+        }
+        userServiceImp.save(user, selectedRoles);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit")
+    public String edit(Model model, @RequestParam("id") Long id) {
+        model.addAttribute("user", userServiceImp.findById(id));
+        model.addAttribute("allRoles", roleService.findAll());
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String update(@ModelAttribute("user") @Valid User user,BindingResult bindingResult,
+                         @RequestParam("id") Long id, @RequestParam("selectedRoles") List<Long> selectedRoles) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
+        userServiceImp.update(user, id, selectedRoles);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+        userServiceImp.delete(id);
+        return "redirect:/admin";
+    }
+
+
+}
